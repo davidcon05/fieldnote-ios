82978f3a370ff82ceb5d43cbd574bff883396053
David Contreras <david.b.contreras@gmail.com>
2026-05-05 15:29:37 -0400

    Initial Commit

---
+++ fieldnoteTests/fieldnoteTests.swift
@@ -0,0 +1,19 @@
+//
+//  fieldnoteTests.swift
+//  fieldnoteTests
+//
+//  Created by David Contreras on 5/5/26.
+//
+
+import Testing
+@testable import fieldnote
+
+struct fieldnoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
